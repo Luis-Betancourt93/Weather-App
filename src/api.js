d6b0d8675454cd8258ec9e80193ea3239a4940df
Luis-Betancourt93 <betancourtluis619@gmail.com>
2023-09-25 21:28:54 -0700

    Added the environment variables

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
+
+
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "", // Add Your key here
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_KEY, // Add Your key here
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // Add your key here
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API; // Add your key here
